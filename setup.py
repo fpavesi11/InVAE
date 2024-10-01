e6209b1199a167356ea8fe701e5657f6782653b8
FedericoPavesi <federico.pavesiwork@gmail.com>
2024-10-01 11:03:23 +0200

    Package fix 0.0.1

--- setup.py
+++ setup.py
@@ -3,17 +3,12 @@ from setuptools import setup, find_packages
 __version__ = '0.1.0'
 
 setup(
-    name='VAENAS',
+    name='InVAE',
     version=__version__,
 
-    url='https://github.com/fpavesi11/NASVAE',
+    url='https://github.com/fpavesi11/InVAE',
     author='Federico Pavesi',
     author_email='f.pavesi11@campus.unimib.it',
 
-    packages=find_packages(include=['vaenas', 
-                                    'vaenas.*',
-                                    'vaenas.decoders',
-                                    'vaenas.flowVAE',
-                                    'vaenas.IAF',
-                                    'vaenas.VanillaVAE']),
+    packages=find_packages(),
 )
